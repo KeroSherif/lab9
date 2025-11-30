732ae66b544933163f11f2435d5b3898f8fd6c00
KeroSherif <kerosherif8@gmail.com>
2025-11-30 15:45:46 +0200

    Update SequentialValidator.java

--- src/SequentialValidator.java
+++ src/SequentialValidator.java
@@ -9,67 +9,69 @@ import java.util.List;
  *
  * @author DANAH
  */
-public class SequentialValidator implements SudokuValidator {
 
+
+public class SequentialValidator implements SudokuValidator {
     @Override
     public ValidationResult validate(int[][] board) {
         List<String> errors = new ArrayList<>();
         boolean valid = true;
 
+        // Check rows
         for (int row = 0; row < 9; row++) {
-            boolean[] seen = new boolean[10];
-
+            boolean[] seen = new boolean[10]; // 1–9 فقط
             for (int col = 0; col < 9; col++) {
                 int num = board[row][col];
-
-                if (seen[num]) {
+                if (num == 0) continue; // تجاهل الخانات الفارغة
+                if (num < 1 || num > 9) {
                     valid = false;
-                    errors.add("Duplicate " + num +
-                               " in ROW " + (row + 1) +
-                               " at column " + (col + 1));
+                    errors.add("Invalid value " + num + " in ROW " + (row + 1) + " at column " + (col + 1));
+                } else if (seen[num]) {
+                    valid = false;
+                    errors.add("Duplicate " + num + " in ROW " + (row + 1) + " at column " + (col + 1));
+                } else {
+                    seen[num] = true;
                 }
-
-                seen[num] = true;
             }
         }
 
+        // Check columns
         for (int col = 0; col < 9; col++) {
             boolean[] seen = new boolean[10];
-
             for (int row = 0; row < 9; row++) {
                 int num = board[row][col];
-
-                if (seen[num]) {
+                if (num == 0) continue;
+                if (num < 1 || num > 9) {
                     valid = false;
-                    errors.add("Duplicate " + num +
-                               " in COLUMN " + (col + 1) +
-                               " at row " + (row + 1));
+                    errors.add("Invalid value " + num + " in COLUMN " + (col + 1) + " at row " + (row + 1));
+                } else if (seen[num]) {
+                    valid = false;
+                    errors.add("Duplicate " + num + " in COLUMN " + (col + 1) + " at row " + (row + 1));
+                } else {
+                    seen[num] = true;
                 }
-
-                seen[num] = true;
             }
         }
 
+        // Check boxes
         for (int boxRow = 0; boxRow < 3; boxRow++) {
             for (int boxCol = 0; boxCol < 3; boxCol++) {
-
                 boolean[] seen = new boolean[10];
                 int startRow = boxRow * 3;
                 int startCol = boxCol * 3;
-
                 for (int r = startRow; r < startRow + 3; r++) {
                     for (int c = startCol; c < startCol + 3; c++) {
-
                         int num = board[r][c];
-
-                        if (seen[num]) {
+                        if (num == 0) continue;
+                        if (num < 1 || num > 9) {
                             valid = false;
-                            errors.add("Duplicate " + num +
-                                       " in BOX (" + (boxRow + 1) + "," + (boxCol + 1) + ")" +
-                                       " at cell (" + (r + 1) + "," + (c + 1) + ")");
+                            errors.add("Invalid value " + num + " in BOX (" + (boxRow + 1) + "," + (boxCol + 1) + ") at cell (" + (r + 1) + "," + (c + 1) + ")");
+                        } else if (seen[num]) {
+                            valid = false;
+                            errors.add("Duplicate " + num + " in BOX (" + (boxRow + 1) + "," + (boxCol + 1) + ") at cell (" + (r + 1) + "," + (c + 1) + ")");
+                        } else {
+                            seen[num] = true;
                         }
-
-                        seen[num] = true;
                     }
                 }
             }
