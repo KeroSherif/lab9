16778986d4309dfe06ca1af4ca87fc8dfac74c0c
monicaazer50-cyber <monicaazer50@gmail.com>
2025-11-30 19:56:23 +0200

    .

--- src/BoxChecker3.java
+++ src/BoxChecker3.java
@@ -7,6 +7,56 @@
  *
  * @author monic
  */
-public class BoxChecker3 {
-    
+public class BoxChecker3 extends Thread {
+    private int[][] board;
+    private int rowStart, colStart;
+    private List<String> errors;
+
+    public BoxChecker3(int[][] board, int rowStart, int colStart, List<String> errors) {
+        this.board = board;
+        this.rowStart = rowStart;
+        this.colStart = colStart;
+        this.errors = errors;
+    }
+
+    @Override
+    public void run() {
+        boolean[] seen = new boolean[9];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                int num = board[rowStart + i][colStart + j];
+                if (num < 1 || num > 9 || seen[num - 1]) {
+                    errors.add("Box starting at (" + (rowStart+1) + "," + (colStart+1) + ") has error");
+                    return;
+                }
+                seen[num - 1] = true;
+            }
+        }
+    }
 }
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/RowChecker3.java
+++ src/RowChecker3.java
@@ -7,6 +7,39 @@
  *
  * @author monic
  */
-public class RowChecker3 {
-    
+import java.util.HashSet;
+import java.util.Set;
+
+public class RowChecker3 extends Thread {
+
+    private int[][] board;
+    private ValidationResult result;
+
+    public RowChecker3(int[][] board) {
+        this.board = board;
+        this.result = new ValidationResult(true, new java.util.ArrayList<>());
+    }
+
+    @Override
+    public void run() {
+        for (int row = 0; row < 9; row++) {
+            Set<Integer> seen = new HashSet<>();
+
+            for (int col = 0; col < 9; col++) {
+                int value = board[row][col];
+
+                if (seen.contains(value)) {
+                    result.addError("Duplicate value " + value + " found in ROW " + row +
+                            " at column " + col);
+                    result = new ValidationResult(false, result.getErrors());
+                } else {
+                    seen.add(value);
+                }
+            }
+        }
+    }
+
+    public ValidationResult getResult() {
+        return result;
+    }
 }
