bde6b77c3549f7e07fa938b158dce0fc85b90473
monicaazer50-cyber <monicaazer50@gmail.com>
2025-12-21 19:00:23 +0200

    >

--- src/SolutionInvalidException.java
+++ src/SolutionInvalidException.java
@@ -2,14 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package lab9;
+
 /**
  *
- * @author DANAH
+ * @author monic
  */
 public class SolutionInvalidException extends Exception {
+
     public SolutionInvalidException(String message) {
         super(message);
     }
-    
 }
