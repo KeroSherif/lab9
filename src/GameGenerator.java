70d10855e62c4469f87fe4f90275edd5a7f9d4ea
danah19112005-lang <danah19112005@gmail.com>
2025-12-21 15:30:04 +0200

    Update GameGenerator.java

--- src/GameGenerator.java
+++ src/GameGenerator.java
@@ -10,7 +10,7 @@ import java.io.File;
  * @author DANAH
  */
 public class GameGenerator {
-    private SudokuVerifier verifier = new SudokuVerifier();
+    private  SudokuVerifier verifier = new SudokuVerifier();
     private StorageManager storage = new StorageManager();
     
  public void generateLevels(String sourcePath) throws SolutionInvalidException {
@@ -27,5 +27,31 @@ public class GameGenerator {
         saveLevel(solvedBoard, 25, DifficultyEnum.HARD, random);
     }
  
+ private void saveLevel(int[][] solved, int cellsToRemove, DifficultyEnum diff, Random rand) {
+        int[][] puzzle = copyBoard(solved);
+        int removed = 0;
+
+        while (removed < cellsToRemove) {
+            int r = rand.nextInt(9);
+            int c = rand.nextInt(9);
+            if (puzzle[r][c] != 0) {
+                puzzle[r][c] = 0; 
+                removed++;
+            }
+        }
+        
+     storage.saveDifficultyFile(puzzle, diff);
+    }
  
+ private int[][] copyBoard(int[][] original) {
+        int[][] copy = new int[9][9];
+        for (int i = 0; i < 9; i++) System.arraycopy(original[i], 0, copy[i], 0, 9);
+        return copy;
+    }
+    
+    private int[][] loadBoardFromFile(String path) {
+        return new int[9][9]; 
+    }
 }
+ 
+
