d2e858150ba6b0421ae35c6c03a4778aea8f0066
danah19112005-lang <danah19112005@gmail.com>
2025-12-21 13:23:12 +0200

    Create CatalogManager.java

---
+++ src/CatalogManager.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab9;
+
+/**
+ *
+ * @author DANAH
+ */
+public class CatalogManager {
+    private final String INCOMPLETE_PATH = "incomplete/";
+    private final String[] MODES = {"easy", "medium", "hard"};
+    
+public Catalog getCatalog() {
+        File incompleteDir = new File(INCOMPLETE_PATH);
+        File[] files = incompleteDir.listFiles();    
+}
