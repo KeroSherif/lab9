892be14c14b9e429b3bb57f6e32e70a76de65925
danah19112005-lang <danah19112005@gmail.com>
2025-12-21 13:40:33 +0200

    Create SudokuVerifier.java

---
+++ src/SudokuVerifier.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab9;
+
+/**
+ *
+ * @author DANAH
+ */
+public class SudokuVerifier {
+    public String verify(int[][] board) {
+        StringBuilder duplicates = new StringBuilder();
+        boolean hasZeros = false;
+}
